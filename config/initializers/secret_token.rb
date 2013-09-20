cccec6ee56dd8596ac2e476bd56121d1c88058f8
Corbin Sykes <corbin.sykes@gmail.comm>
2013-09-20 15:29:38 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babble::Application.config.secret_token = '34e7594dba2a54047c67549c3aa642eb95739a49f1cfe30c90c21f6b5341b62ca1ed554694d55feabbda2a99c4938237cb0f15dcdd04ceb3f0c09a60850279c2'
