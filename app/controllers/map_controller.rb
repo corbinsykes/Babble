f262969e7fb394c3652e8ffca6193d1a3b26a186
Corbin Sykes <corbin.sykes@gmail.comm>
2013-10-05 16:19:01 -0400

    banging head against wall

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,10 +1,16 @@
 class MapController < ApplicationController
 
   def index
-    @tweet = Tweet.first
-    @lat = Tweet.first(params[:lat])
-    @lng = Tweet.first(params[:lng])
-    @lang = Tweet.first(params[:lang])
   end
 
+  def tweets
+    @tweets = Tweet.all
+    @lat = Tweet(params[:lat])
+    @lng = Tweet(params[:lng])
+    @lang = Tweet(params[:lang])
+    respond_to do |format|
+      format.html
+      format.json {render json: @tweets}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Babble::Application.routes.draw do
   get '/' => 'map#index'
+  get '/tweets' => 'map#tweets'
 end
